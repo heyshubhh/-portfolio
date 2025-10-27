09f3ca97ba75a07a7677ac32a88633f17759a389
heyshubhh <hey.shubhh@gmail.com>
2025-10-27 12:29:39 +0530

    initial changes

---
+++ script.js
@@ -0,0 +1,6 @@
+
+ 
+    // Smooth scroll when "Enroll Your Child" clicked
+    document.querySelector('.about button').addEventListener('click', ()=>{
+      document.getElementById('applySection').scrollIntoView({behavior:'smooth'});
+    });
